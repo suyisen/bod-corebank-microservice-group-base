c23f6bbbbf6148c8bd695a4fcda7581489f3ba67
suyisen <suyisen@163.com>
2019-09-24 00:34:26 +0800

    123

---
+++ bod-corebank-microservice-group-base/src/main/java/org/suyisen/bod/corebank/microservice/group/base/BaseApplication.java
@@ -0,0 +1,13 @@
+package org.suyisen.bod.corebank.microservice.group.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApplication {
+
+	
+	 public static void main(String[] args) {
+	        SpringApplication.run(BaseApplication.class, args);
+	    }
+}
